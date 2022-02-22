e28d1a670e0e3b6feb8680cce910702eba54c963
Your Name <you@example.com>
2022-02-22 11:00:05 +0200

    deploy --profile default

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -7,9 +7,9 @@ eb create Ebhostingafullstackapplication-env-1
 # eb deploy Ebhostingafullstackapplication-env-1
 
 #!/bin/bash
-eb init Ebhostingafullstackapplication-env --platform node.js --region us-east-1 
-eb use Ebhostingafullstackapplication-env --profile default
-eb deploy --profile default 
+eb init uda-api-dev --platform node.js --region us-east-1 
+# eb use uda-api-dev --profile default
+# eb deploy --profile default 
 
 
-eb setenv AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" POSTGRES_USERNAME="$POSTGRES_USERNAME" POSTGRES_PASSWORD="$POSTGRES_PASSWORD" POSTGRES_DB="$POSTGRES_DB" PORT="$PORT" POSTGRES_HOST="$POSTGRES_HOST" RDS_DIALECT="$RDS_DIALECT" AWS_REGION="$AWS_REGION" AWS_PROFILE="$AWS_PROFILE" AWS_BUCKET="$AWS_BUCKET" URL="$URL" DB_PORT="$DB_PORT" JWT_SECRET="$JWT_SECRET" --profile default
+eb use uda-api-dev && eb deploy && eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET 
