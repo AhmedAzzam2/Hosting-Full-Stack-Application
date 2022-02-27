85c251544b45b1265299bd1df898044916f465f4
Your Name <you@example.com>
2022-02-27 23:11:08 +0200

    bcrypt

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -10,9 +10,7 @@ export const config = {
   port_db: Number(process.env.DB_PORT),
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
